f0fe2db3a5ba734fd9a3ff02105dc5853bee5935
Roydan Chu <roydanchu@gmail.com>
2021-01-07 11:25:39 -0800

    Random events created

--- main.py
+++ main.py
@@ -11,17 +11,48 @@ import time
 
 INTRODUCTION = """
 WELCOME TO MIDNIGHT RIDER
-WE'VE STOLEN A CAR. WE NEED TO GET IT HOME.
-THE CAR IS SPECIAL.
-THAT'S WHY THE GOVERNMENT WANTS IT.
-WE CAN'T LET THEM HAVE IT.
-ONE GOAL: SURVIVAL... and THE CAR
-REACH THE END BEFORE THE MAN GON GETCHU.
-------
+
+WE'VE STOLEN A CAR. WE NEED TO GET IT HOME. 
+THE CAR IS SPECIAL. 
+THAT'S WHY THE GOVERNMENT WANTS IT. 
+WE CAN'T LET THEM HAVE IT. 
+ONE GOAL: SURVIVAL... and THE CAR 
+REACH THE END BEFORE THE MAN GON GETCHU. 
+------ 
+"""
+
+WIN = """ 
+YOU PRESSED THE BUTTON TO OPEN THE GATE. 
+THIS ISN'T THE FIRST TIME YOU'VE DONE THIS.  
+YOUN CAN TIME IT PERFECTLY SO THAT YOU  
+SLIDE THE CAR IN AS THE GATES CLOSE. 
+ 
+YOU KNOW YOU DID THE RIGHT THING. 
+THE GOVERNMENT WOULD HAVE TORN THE CAR APART. 
+ANALYSING IT, TESTING IT, THEN DESTROYING IT. 
+ 
+THEY DON'T KNOW ITS SECRETS... 
+THAT IT HOLDS THE KEY TO DIFFERENT WORLDS. 
+ 
+AS YOU STEP OUT OF THE VEHICLE, FIDO RUNS  
+UP TO YOU. 
+"THANK YOU FOR SAVING ME," HE SAYS. 
+ 
+AS YOU TAKE A COUPLE OF STEPS AWAY FROM THE CAR 
+IT MAKES A STRANGE NOISE
+
+BEFORE YOUR EYES, IT SHIFTS ITS SHAPE.
+YOU'VE SEEN IT BEFORE, BUT ONLY ON TV.
+
+"BUMBLEBEE...?"
+
+----GAME OVER----
 """
 
 CHOICES = """
     ----
+    A. Eat a piece of tofu    
+    B. Drive at a moderate speed
     C. Speed ahead at full throttle
     D. Stop to refuel (NO FOOD AVAILABLE)
     E. Status check    
@@ -30,8 +61,8 @@ CHOICES = """
 """
 
 
-def intro():
-    for char in textwrap.dedent(INTRODUCTION):
+def type_text_output(string):
+    for char in textwrap.dedent(string):
         time.sleep(0.05)
         sys.stdout.write(char)
         sys.stdout.flush()
@@ -40,31 +71,82 @@ def intro():
 
 
 def main():
-    # intro()
+    #type_text_output(INTRODUCTION)
 
     # CONSTANTS
     MAX_FUEL_LEVEL = 50
+    MAX_DISTANCE_TRAVELLED = 100
+    MAX_TOFU = 3
 
     # Variables
     done = False
 
-    kms_travelled = 0       # 100km is the end
+    kms_travelled = 99       # 100km is the end
     agents_distance = -20   # 0 is the end
     turns = 0
-    tofu = 3                # 3 is max
+    tofu = MAX_TOFU         # 3 is max
     fuel = MAX_FUEL_LEVEL   # max is 50L
     hunger = 0
 
     # MAIN LOOP
     while not done:
-        # TODO: Check if reached END GAME
+        # Random events
+        # FIDO - refills your food (5%)
+        if tofu < 3 and random.random() < 0.05:
+            # refill tofu
+            tofu = MAX_TOFU
+            # player feedback
+            print("******** You look at your tofu container.")
+            print("******** It is filled magically.")
+            print("******** \"You're welcome!\", says a small voice.")
+            print("******** The dog used its magic tofu cooking skills.")
+
+        # Check if reached END GAME
+        # WIN - Travelled the distance required
+        if kms_travelled > MAX_DISTANCE_TRAVELLED:
+            time.sleep(2)
+            type_text_output(WIN)
+            break
 
         # Present the user their choices
         print(CHOICES)
 
         user_choice = input("What do you want to do? ").lower().strip("!,.? ")
 
-        if user_choice == "c":
+        if user_choice == "a":
+            # Eat/HUNGER
+            if tofu > 0:
+                tofu -= 1
+                hunger = 0
+                print()
+                print("-------- Mmmmm, soybean goodness.:)")
+                print("-------- Your hunger is sated.")
+                print()
+            else:
+                print()
+                print("-------- You have none left.")
+                print()
+
+        elif user_choice == "b":
+            # Moderate
+            players_distance_now = random.randrange(7, 13)
+            agents_distance_now = random.randrange(7, 13)
+
+            # Burn fuel
+            fuel -= random.randrange(3,8)
+
+            # Player distance travelled
+            kms_travelled += players_distance_now
+
+            # Agents distance travelled
+            agents_distance -= players_distance_now - agents_distance_now
+
+            # Feedback to the player
+            print()
+            print(f"-------- You travelled {players_distance_now}kms.")
+            print()
+
+        elif user_choice == "c":
             # FAST
             players_distance_now = random.randrange(10, 16)
             agents_distance_now = random.randrange(7, 15)
@@ -80,7 +162,7 @@ def main():
 
             # Feedback to the player
             print()
-            print("ZOOOM")
+            print("-------- ZOOOM")
             print(f"-------- You travelled {players_distance_now}kms. ")
             print()
 
@@ -100,11 +182,11 @@ def main():
 
         elif user_choice == "e":
             print(f"\t---Status Check---")
-            print("f\tkm travelled: {kms_travelled}")
-            print(f"f\tFuel remaining: {fuel}L")
-            print(f"f\tAgents are {abs(agents_distance)} kms behind.")
-            print(f"f\tYou have {tofu} tofu left.")
-            print("f\t----------\n")
+            print(f"\tkm travelled: {kms_travelled}")
+            print(f"\tFuel remaining: {fuel}L")
+            print(f"\tAgents are {abs(agents_distance)} kms behind.")
+            print(f"\tYou have {tofu} tofu left.")
+            print("\t----------\n")
         elif user_choice == "q":
             done = True
 
